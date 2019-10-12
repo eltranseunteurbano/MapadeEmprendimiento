b4964a4b80d5c4a80cc181c713b10da38dc1b20c
Jaime Burbano <burbanojaime98@gmail.com>
2019-10-12 09:33:55 -0500

    Updates

---
+++ precache-manifest.015305950929cb2f41ccbf1017f43c26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e584f198cb711bd5721328f8b83ee97",
+    "url": "/MapadeEmprendimiento/index.html"
+  },
+  {
+    "revision": "38185aa366591a122532",
+    "url": "/MapadeEmprendimiento/static/css/main.b5ec655c.chunk.css"
+  },
+  {
+    "revision": "6d587df151d7634cc869",
+    "url": "/MapadeEmprendimiento/static/js/2.9a0aab7e.chunk.js"
+  },
+  {
+    "revision": "38185aa366591a122532",
+    "url": "/MapadeEmprendimiento/static/js/main.30fe3bc7.chunk.js"
+  },
+  {
+    "revision": "cbad1bd63c3a5118a325",
+    "url": "/MapadeEmprendimiento/static/js/runtime-main.11b2dea3.js"
+  },
+  {
+    "revision": "82addd02da227b96c97d09b9ed7e58de",
+    "url": "/MapadeEmprendimiento/static/media/Background Image.82addd02.png"
+  }
+]);
